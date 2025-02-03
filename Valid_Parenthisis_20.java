e5345b6c4690e647e353a426336348af065328c9
Suman <sumannayak192113114@gmail.com>
2025-02-04 00:16:30 +0530

    update 20

--- Valid_Parenthisis_20.java
+++ Valid_Parenthisis_20.java
@@ -4,32 +4,36 @@ import java.util.Stack;
 
 public class Valid_Parenthisis_20 {
 	public static void main(String[] args) {
-		String str="[({}){}]";
+		String str = "){";
 		boolean valid = isValid(str);
 		System.out.println(valid);
 	}
-	public static boolean isValid(String str){
-		if (str.isEmpty())return false;
-		Stack<Character>s=new Stack<>();
-		for(char i:str.toCharArray()){
+
+	public static boolean isValid(String str) {
+		if (str.length() < 2) return false;
+		Stack<Character> s = new Stack<>();
+		for (char i : str.toCharArray()) {
 //			System.out.println(i);
-			if (i == '[' || i =='{' || i == '('){
+
+			if (i == '[' || i == '{' || i == '(') {
 				s.push(i);
-			}else{
-				switch (i){
+			} else if (i == ']' || i == '}' || i == ')') {
+				if (s.isEmpty()) return false;
+				switch (i) {
 					case ')':
-						if (s.pop()!='(') return false;
-						break;
+						if (s.pop() != '(') return false;
+						continue;
 					case '}':
-						if (s.pop()!='{') return false;
-						break;
+						if (s.pop() != '{') return false;
+						continue;
 					case ']':
-						if (s.pop()!='[') return false;
-						break;
+						if (s.pop() != '[') return false;
+
 				}
 			}
 		}
-
-		return true;
+		if (s.isEmpty()) {
+			return true;
+		} else return false;
 	}
 }
