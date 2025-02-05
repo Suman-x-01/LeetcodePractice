2881fb6fb25439dc8b28a4d222b79a09d7dd4b90
Suman <sumannayak192113114@gmail.com>
2025-02-05 22:26:14 +0530

    update 6

--- ZigzagConversion_6.java
+++ ZigzagConversion_6.java
@@ -2,40 +2,47 @@ package com.LeetCode;
 
 public class ZigzagConversion_6 {
 	public static void main(String[] args) {
-		String str="1234567890";
-		int numofrows=4;
-		convert(str, numofrows);
+		String str="PAYPALISHIRING";
+		int numofrows=3;
+		String convert = convert(str, numofrows);
+		System.out.println(convert);
 	}
 
-	public static void convert(String str, int numRows) {
-//		1234567890
-		int vertical=0,res=0;
-		res= numRows-3;
-		vertical=res +numRows;
-		for (int i = 0; i < numRows; i++) {
-			System.out.print(str.charAt(i));
-//			printing the space
-//			for ( int j=0;j<vertical-(i+2);j++){
-			int j=0;
-				while (j<=vertical-(i+2)){
-					j++;
-				System.out.print(" ");
-				if (j==vertical-(i+2) && i!=0){
-					System.out.print(str.charAt((vertical+1)-i));
-				}
-			}
+	public static String convert(String s, int numRows) {
+		// If there's only one row, return the string as is.
+		if (numRows == 1) return s;
 
+		// Create a list to hold each row of the zigzag pattern.
+		StringBuilder[] rows = new StringBuilder[Math.min(numRows, s.length())];
 
-			for (int k = 1; k <=i-1; k++) {
-//				if (i==1 ||i>1){
-
-				System.out.print(" ");
-//				}
-			}
-				System.out.print(str.charAt(vertical+1+i));
-//			}
-			System.out.println();
+		// Initialize each row as an empty StringBuilder.
+		for (int i = 0; i < rows.length; i++) {
+			rows[i] = new StringBuilder();
 		}
 
+		int currentRow = 0;
+		boolean goingDown = false;
+
+		// Traverse the string and place characters in the appropriate row.
+		for (char c : s.toCharArray()) {
+			rows[currentRow].append(c);
+
+			// Change direction when we reach the first or last row.
+			if (currentRow == 0 || currentRow == numRows - 1) {
+				goingDown = !goingDown;
+			}
+
+			// Move to the next row (up or down).
+			currentRow += goingDown ? 1 : -1;
+		}
+
+		// Combine all rows into a single string.
+		StringBuilder result = new StringBuilder();
+		for (StringBuilder row : rows) {
+			result.append(row);
+		}
+
+		return result.toString();
+//		System.out.println(rows.toString());
 	}
 }
